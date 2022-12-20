1d64b9f5fd2a9b752a6a66deeef49593b3307b32
Nicolas Bonnec <nicolas.bonnec@cp.com>
2022-12-20 14:00:30 +0100

    Day 8 part two, it was an horrible moment. No pleasure.

--- 8/main.cpp
+++ 8/main.cpp
@@ -1,5 +1,7 @@
 #include <algorithm>
 #include <filesystem>
+#include <fmt/core.h>
+#include <fmt/ranges.h>
 #include <fstream>
 #include <iostream>
 #include <ranges>
@@ -8,6 +10,7 @@
 #include <vector>
 
 #include "config.hpp"
+#include "misc.hpp"
 
 namespace fs = std::filesystem;
 namespace rng = std::ranges;
@@ -20,20 +23,21 @@ static std::ifstream getFile() {
 	return {filePath, std::ios::in};
 }
 
-static int partOne() {
-	auto ifs = getFile();
+using Trees = std::vector<std::vector<int>>;
+using Coordinates = std::pair<int, int>;
+using Visible = std::vector<Coordinates>;
 
-	std::cout << '\n';
-
-	std::vector<std::vector<int>> trees;
-	for (std::string line; std::getline(ifs, line);) {
-		std::vector<int> lineTrees;
-		rng::transform(line, std::back_inserter(lineTrees), [](auto c) -> int { return c - '0'; });
-		trees.emplace_back(lineTrees);
+static void drawVisible(const Trees& trees, const Visible& visible) { // Draw the visible trees!
+	for (int r{}; r < trees.size(); r++) {
+		for (int c{}; c < trees[r].size(); c++) {
+			const auto v =
+				rng::find_if(visible, [r, c](const auto& pair) { return pair.first == r && pair.second == c; });
+			std::cout << (v == visible.end() ? ' ' : '*');
+		}
+		std::cout << '\n';
 	}
-
-	using Coordinates = std::pair<int, int>;
-	std::vector<Coordinates> visible{};
+}
+static int partOne(const Trees& trees, Visible& visible) {
 
 	// Borders are all visible
 	for (auto row : vws::iota(0, int(trees.size()))) {
@@ -128,25 +132,98 @@ static int partOne() {
 	}
 	const auto s = std::set(visible.begin(), visible.end());
 
-	// Draw the visible trees!
-	for (int r{}; r < trees.size(); r++) {
-		for (int c{}; c < trees[r].size(); c++) {
-			const auto v =
-				rng::find_if(visible, [r, c](const auto& pair) { return pair.first == r && pair.second == c; });
-			std::cout << (v == visible.end() ? ' ' : '*');
-		}
-		std::cout << '\n';
-	}
+	drawVisible(trees, visible);
 
 	return s.size();
 }
 
-static int partTwo() {
-	return 0;
+static int partTwo(const Trees& trees, const Visible& visible) {
+	std::vector<std::size_t> scenicViews{};
+	for (int rowIndex{}; rowIndex < trees.size(); rowIndex++) {
+		const auto& row = trees[rowIndex];
+		for (int columnIndex{}; columnIndex < row.size(); columnIndex++) {
+			const auto rowIt = row.begin() + columnIndex;
+			fmt::print("Row {}, column {}, value {}\n", rowIndex, columnIndex, *rowIt);
+
+			std::size_t scenicView{1};
+			// Looking up is like looking left in the column
+			{
+				int count{};
+				if (rowIndex >= 0) {
+					for (int r = rowIndex - 1; r >= 0; r--) {
+						count++;
+						if (*rowIt <= trees[r][columnIndex]) {
+							break;
+						}
+					}
+				}
+				fmt::print("up {}\n", count);
+				scenicView *= count;
+			}
+			// Looking left
+			{
+				int count{};
+				if (columnIndex >= 0) {
+					for (int c = columnIndex - 1; c >= 0; c--) {
+						count++;
+						if (*rowIt <= trees[rowIndex][c]) {
+							break;
+						}
+					}
+				}
+				fmt::print("left {}\n", count);
+				scenicView *= count;
+			}
+			// Looking right
+			{
+				int count{};
+				if (columnIndex < row.size() - 1) {
+					for (int c = columnIndex + 1; c < row.size(); c++) {
+						count++;
+						if (*rowIt <= trees[rowIndex][c]) {
+							break;
+						}
+					}
+				}
+				fmt::print("right {}\n", count);
+				scenicView *= count;
+			}
+
+			// Looking down is like looking right in the column
+			{
+				int count{};
+				if (rowIndex < row.size() - 1) {
+					for (int r = rowIndex + 1; r < trees.size(); r++) {
+						count++;
+						if (*rowIt <= trees[r][columnIndex]) {
+							break;
+						}
+					}
+				}
+				fmt::print("down {}\n", count);
+				scenicView *= count;
+			}
+			fmt::print("\n");
+			scenicViews.emplace_back(scenicView);
+		}
+	}
+	return *rng::max_element(scenicViews);
 }
 
 int main() {
-	std::cout << "Part one: " << partOne() << '\n';
-	std::cout << "Part two: " << partTwo() << '\n';
+	auto ifs = getFile();
+
+	Trees trees;
+
+	for (std::string line; std::getline(ifs, line);) {
+		std::vector<int> lineTrees;
+		rng::transform(line, std::back_inserter(lineTrees), [](auto c) -> int { return c - '0'; });
+		trees.emplace_back(lineTrees);
+	}
+
+	Visible visible{};
+
+	//	std::cout << "Part one: " << partOne(trees, visible) << '\n';
+	std::cout << "Part two:\n " << partTwo(trees, visible) << '\n';
 	return 0;
 }
