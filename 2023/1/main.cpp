a178e30980ce36ee793573b6df306eca9c97787e
nbonnec <nope@gmail.com>
2023-12-09 12:33:11 +0100

    Success!

--- 2023/1/main.cpp
+++ 2023/1/main.cpp
@@ -1,15 +1,18 @@
 
-#include <charconv>
+#include <algorithm>
 #include <filesystem>
 #include <fstream>
-#include <future>
-#include <iostream>
+#include <numeric>
+#include <ranges>
 #include <source_location>
 
 #include "config.hpp"
 #include "fmt/core.h"
 
 namespace fs = std::filesystem;
+namespace rg = std::ranges;
+namespace wvs = std::views;
+using namespace std::string_view_literals;
 
 static std::ifstream getFile() {
 	const auto thisFileName = std::source_location::current().file_name();
@@ -19,29 +22,18 @@ static std::ifstream getFile() {
 }
 
 static void partOne() {
+	auto combine = [](const int acc, std::string line) {
+		static constexpr auto digits = "0123456789"sv;
+		const auto first = rg::find_first_of(line, digits);
+		const auto last = rg::find_first_of(line | wvs::reverse, digits);
+		return acc + stoi(fmt::format("{}{}", *first, *last));
+	};
+
 	auto ifs = getFile();
-	for (std::string line; std::getline(ifs, line);) {
-		fmt::print("Line is {}\n", line);
-		int firstDigit{};
-		int secondDigit{};
-		std::string combine{};
-		auto [ptr, ec] = std::from_chars(line.c_str(), line.c_str() + line.size(), firstDigit);
-		const auto remainder = std::string{ptr};
-		fmt::print("Remainder is {}\n", remainder);
 
-		if (auto [ptr2, ec2] = std::from_chars(remainder.c_str(), remainder.c_str() + remainder.size(), secondDigit);
-			ec2 != std::errc{}) {
-			std::cout << "Errc= " << std::make_error_code(ec2) << '\n';
-			std::cout << ptr2;
-			secondDigit = firstDigit;
-		}
-
-		fmt::print("First digit= {}\n", firstDigit);
-		fmt::print("Second digit= {}\n", secondDigit);
-
-		combine = fmt::format("{}{}", firstDigit, secondDigit);
-		fmt::print("Combine is {}\n", combine);
-	}
+	const auto sum =
+		std::accumulate(std::istream_iterator<std::string>(ifs), std::istream_iterator<std::string>{}, 0, combine);
+	fmt::print("Sum is {}\n", sum);
 }
 
 int main() {
